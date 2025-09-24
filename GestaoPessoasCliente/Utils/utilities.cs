3b0cd6a55a319a884664ed6d40ee0cc9df8a7965
Vasco Apolinário <vascoapolinario@gmail.com>
2025-09-24 16:39:34 +0100

    Remoção do formulario id para adições
    Ao adicionar um novo trabalhador já não é pedido o parametro id, este parametro apenas é pedido quando se atualiza um trabalhador. #4

--- GestaoPessoasCliente/Utils/utilities.cs
+++ GestaoPessoasCliente/Utils/utilities.cs
@@ -5,11 +5,14 @@ namespace GestaoPessoasCliente.Utils
 {
     public class WorkerUtilities
     {
-        public Worker InputWorker()
+        public Worker InputWorker(bool NewWorker)
         {
             Console.WriteLine("Digite os detalhes do trabalhador:");
-            Console.Write("Id: ");
-            int id = Convert.ToInt32(Console.ReadLine());
+            int id = 0;
+            if (!NewWorker) {
+                Console.Write("Id: ");
+                id = Convert.ToInt32(Console.ReadLine());
+            }
             Console.Write("Nome: ");
             string name = Console.ReadLine();
             Console.Write("Data de Nascimento (yyyy-MM-dd): ");
